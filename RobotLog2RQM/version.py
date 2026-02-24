5afdc60a5ce785cad09cf04c993ba314dda650d6
qth2gi <Holger.Queckenstedt@de.bosch.com>
2026-02-24 16:37:09 +0100

    Usage of Setuptools replaced by usage of PIP/TOML/Setuptools

--- RobotLog2RQM/version.py
+++ RobotLog2RQM/version.py
@@ -1,6 +1,6 @@
 # **************************************************************************************************************
 #
-#  Copyright 2020-2023 Robert Bosch GmbH
+#  Copyright 2020-2026 Robert Bosch GmbH
 #
 #  Licensed under the Apache License, Version 2.0 (the "License");
 #  you may not use this file except in compliance with the License.
@@ -18,5 +18,5 @@
 #
 # Version and date of RobotLog2RQM
 #
-VERSION      = "1.7.1"
-VERSION_DATE = "03.02.2026"
+VERSION      = "1.8.0"
+VERSION_DATE = "24.02.2026"
